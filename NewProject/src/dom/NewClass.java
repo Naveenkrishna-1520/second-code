76275bbf1356af75f22a951b77577eaeddc0ea2c
M1064395 <M1064395@MT-PG02AL1J.Dlink>
2021-06-03 10:29:16 +0530

    new one

---
+++ NewProject/src/dom/NewClass.java
@@ -0,0 +1,17 @@
+package dom;
+
+import java.util.Scanner;
+
+public class NewClass {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println("enter number");
+		int n = sc.nextInt();
+		if (n == 3) {
+			System.out.println("Go to review");
+		} else {
+			System.out.println("Drop the review");
+		}
+	}
+}
+
